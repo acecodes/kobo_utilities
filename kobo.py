eb01d28d71ae2cd891edd1683338425e8b542a3c
Ace <ace.github@gmail.com>
2016-08-29 11:43:45 -0700

    Made DB functions staticmethods, added get and push to download/upload DB files directly from a Kobo device

--- kobo.py
+++ kobo.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
 import sqlite3
+import os
 
 from sys import argv
 from random import shuffle
-
+from shutil import copy2
 
 class KoboDB:
-    def __init__(self, db_file):
-        self.db_file = db_file
-
-    def get_highlights(self, reverse=False, random=False):
-
-        db_file = self.db_file
+    
+    @staticmethod
+    def get_highlights(db_file, reverse=False, random=False):
 
         db = sqlite3.connect(db_file)
         cursor = db.cursor()
@@ -49,17 +47,32 @@ class KoboDB:
 
 if __name__ == '__main__':
 
-    kobo = KoboDB('KoboReader.sqlite')
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     try:
-        args = argv[1]
+        sqlite_file = 'KoboReader.sqlite'
+        behavior_arg = argv[1]
 
-        if args == 'reverse':
-            kobo.get_highlights(reverse=True)
-        if args == 'random':
-            kobo.get_highlights(random=True)
-        else:
-            kobo.get_highlights()
+        if behavior_arg == 'reverse':
+            KoboDB.get_highlights(sqlite_file, reverse=True)
+        if behavior_arg == 'random':
+            KoboDB.get_highlights(sqlite_file, random=True)
+        if behavior_arg == 'get':
+            get_decide = input('This will copy your Kobo\'s DB to disk. Are you sure you want to proceed? (y/n) ')
+            if get_decide == 'y':
+                print('Copying your Kobo\'s SQLite file to this directory...')
+                copy2('/Volumes/KOBOeReader/.kobo/KoboReader.sqlite', BASE_DIR + '/KoboReader.sqlite')
+                print('Done copying.')
+            else:
+                pass
+        if behavior_arg == 'push':
+            push_decide = input('This will copy your local DB to your Kobo. Are you sure you want to proceed? (y/n) ')
+            if push_decide == 'y':
+                print('Copying your local Kobo SQLite file to your Kobo...')
+                copy2(BASE_DIR + '/KoboReader.sqlite', '/Volumes/KOBOeReader/.kobo/KoboReader.sqlite')
+                print('Done copying SQLite file to your Kobo.')
+            else:
+                pass
 
     except IndexError:
-        kobo.get_highlights()
+        KoboDB.get_highlights(sqlite_file)
