c3afc3fe5ac8b7b49f1d640d227db01c9037750e
Ace <ace.github@gmail.com>
2016-08-30 10:00:04 -0700

    Now works with 2.x, put get and push features into their own staticmethods

--- kobo.py
+++ kobo.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import print_function
 
 import sqlite3
 import os
@@ -7,8 +8,15 @@ from sys import argv
 from random import shuffle
 from shutil import copy2
 
+# Backward compatibility
+try:
+    input = raw_input
+except NameError:
+    pass
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 class KoboDB:
-    
     @staticmethod
     def get_highlights(db_file, reverse=False, random=False):
 
@@ -26,7 +34,9 @@ class KoboDB:
         if random:
             shuffle(bookmarks)
 
-        while count < len(bookmarks):
+        bookmarks_length = len(bookmarks)
+
+        while count < bookmarks_length:
             print('\n', bookmarks[count][1], '\n\n', bookmarks[count][2], '\n')
             decide = str(input('Do you want to keep this bookmark (y/n/stop)? '))
             if decide == 'n':
@@ -44,10 +54,28 @@ class KoboDB:
         print('Closing connection to DB...')
         db.close()
 
+    @staticmethod
+    def get_db():
+        get_decide = input('This will copy your Kobo\'s DB to disk. Are you sure you want to proceed? (y/n) ')
+        if get_decide == 'y':
+            print('Copying your Kobo\'s SQLite file to this directory...')
+            copy2('/Volumes/KOBOeReader/.kobo/KoboReader.sqlite', BASE_DIR + '/KoboReader.sqlite')
+            print('Done copying.')
+        else:
+            pass
+
+    @staticmethod
+    def push_db():
+        push_decide = input('This will copy your local DB to your Kobo. Are you sure you want to proceed? (y/n) ')
+        if push_decide == 'y':
+            print('Copying your local Kobo SQLite file to your Kobo...')
+            copy2(BASE_DIR + '/KoboReader.sqlite', '/Volumes/KOBOeReader/.kobo/KoboReader.sqlite')
+            print('Done copying SQLite file to your Kobo.')
+        else:
+            pass
+
 
 if __name__ == '__main__':
-
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
     try:
         sqlite_file = 'KoboReader.sqlite'
@@ -59,21 +87,9 @@ if __name__ == '__main__':
         elif behavior_arg == 'random':
             KoboDB.get_highlights(sqlite_file, random=True)
         elif behavior_arg == 'get':
-            get_decide = input('This will copy your Kobo\'s DB to disk. Are you sure you want to proceed? (y/n) ')
-            if get_decide == 'y':
-                print('Copying your Kobo\'s SQLite file to this directory...')
-                copy2('/Volumes/KOBOeReader/.kobo/KoboReader.sqlite', BASE_DIR + '/KoboReader.sqlite')
-                print('Done copying.')
-            else:
-                pass
+            KoboDB.get_db()
         elif behavior_arg == 'push':
-            push_decide = input('This will copy your local DB to your Kobo. Are you sure you want to proceed? (y/n) ')
-            if push_decide == 'y':
-                print('Copying your local Kobo SQLite file to your Kobo...')
-                copy2(BASE_DIR + '/KoboReader.sqlite', '/Volumes/KOBOeReader/.kobo/KoboReader.sqlite')
-                print('Done copying SQLite file to your Kobo.')
-            else:
-                pass
+            KoboDB.push_db()
 
     except IndexError:
         KoboDB.get_highlights(sqlite_file)
